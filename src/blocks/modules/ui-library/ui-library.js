dadcce75360ebd2624c9633398b12a71b5d48580
kenny_nobody <nobodykenny@gmail.com>
2020-04-16 22:38:38 +0500

    'work'

--- src/blocks/modules/ui-library/ui-library.js
+++ src/blocks/modules/ui-library/ui-library.js
@@ -2,23 +2,101 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/ru';
 
-(function() {
+document.addEventListener("DOMContentLoaded", ready);
 
-	document.addEventListener('DOMContentLoaded', function() {
-		var calendarEl = document.querySelector('.calendar');
+function ready() {
 
-		var calendar = new Calendar(calendarEl, {
-			plugins: [ dayGridPlugin ],
-			locale: 'ru',
-			weekNumberCalculation: 'ISO',
-			header: {
-				left:   'prev, title, next',
-				center: '',
-				right:  'today'
+	(function() {
+
+		const calendarArrow = document.querySelectorAll('.calendar');
+
+		if (calendarArrow) {
+			for(let i = 0; i < calendarArrow.length; i++) {
+				let calendar = new Calendar(calendarArrow[i], {
+					plugins: [ dayGridPlugin ],
+					locale: 'ru',
+					weekNumberCalculation: 'ISO',
+					header: {
+						left:   'prev, title, next',
+						center: '',
+						right:  'today'
+					}
+				});
+
+				calendar.render();
 			}
-		});
+		}
 
-		calendar.render();
-	});
+	})();
 
-})();
+	// Выпадение дроп-карточки
+
+	(function () {
+		const dropArrow = document.querySelectorAll('.drop-card__header');
+
+		if (dropArrow) {
+			for (let i = 0; i < dropArrow.length; i++) {
+				dropArrow[i].addEventListener('click', function() {
+					this.parentNode.classList.toggle('drop-card--open');
+					console.log(this.parentNode)
+				})
+
+				dropArrow[i].addEventListener('keydown', function() {
+					if (event.code == 'Space') {
+						event.preventDefault();
+						this.parentNode.classList.toggle('drop-card--open');
+					}
+				})
+			}
+		}
+
+	})();
+
+	// Переключения radio с клавиатуры
+
+	(function () {
+		const radio = document.querySelectorAll('.radio');
+
+		if (radio) {
+			for (let i = 0; i < radio.length; i++) {
+				radio[i].addEventListener('keydown', setCustomEvent);
+			}
+		}
+
+		function setCustomEvent () {
+			if (event.code == 'Space') {
+				event.preventDefault();
+				this.querySelector('input').checked = true;
+			}
+		}
+
+	})();
+
+	// Переключения checkbox с клавиатуры
+
+	(function () {
+		const radio = document.querySelectorAll('.checkbox');
+
+		if (radio) {
+			for (let i = 0; i < radio.length; i++) {
+				radio[i].addEventListener('keydown', setCustomEvent);
+			}
+		}
+
+		function setCustomEvent () {
+			if (event.code == 'Space') {
+				event.preventDefault();
+				if (this.querySelector('input'). checked == true) {
+					this.querySelector('input').checked = false;
+				} else {
+					this.querySelector('input').checked = true;
+				}
+				
+			}
+		}
+
+	})();
+
+
+}
+
