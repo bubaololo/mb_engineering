677ab2c23a380ead666b5202204b57a74d2770f4
kenny_nobody <nobodykenny@gmail.com>
2020-05-12 18:18:44 +0500

    'work'

--- src/blocks/modules/sliders/sliders.js
+++ src/blocks/modules/sliders/sliders.js
@@ -1,8 +1,27 @@
 import Swiper from 'swiper';
 
 (function () {
+	const mySwiper = new Swiper ('.slider-front__container', {
+		effect: 'fade',
+		pagination: {
+			el: '.slider-front__fraction',
+			type: 'fraction',
+			renderFraction: function (currentClass, totalClass) {
+				return '<div class="slider-front__now ' + currentClass + '"></div>' +
+				'<div class="slider-front__sep"></div>' +
+				'<div class="slider-front__all ' + totalClass + '"></div>';
+			}
+		},
+		navigation: {
+			nextEl: '.slider-front__btn--next',
+			prevEl: '.slider-front__btn--prev',
+			disabledClass: 'slider-front__btn--disabled'
+		},
+	})
+})();
+
+(function () {
 	const mySwiper = new Swiper ('.slider-page__container', {
-		// slideActiveClass: 'lalala',
 		effect: 'fade',
 		pagination: {
 			el: '.slider-page__fraction',
